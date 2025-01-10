24a28700b3e0d15e14bd9dc56f78f331d4f8fd43
Sophia <136660013+sophmi@users.noreply.github.com>
2025-01-10 08:29:40 +0000

    Reorder gradle configuration

--- gradle/convention-plugins/src/main/kotlin/kotlin-conventions.gradle.kts
+++ gradle/convention-plugins/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -1,7 +1,9 @@
 import org.gradle.accessors.dm.LibrariesForLibs
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsEnvSpec
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
 plugins {
 	kotlin("multiplatform")
@@ -35,13 +37,13 @@ kotlin {
 			implementation(kotlin("test-common"))
 		}
 
+		jsMain.dependencies {
+			implementation(kotlin("stdlib-js"))
+		}
+
 		jvmTest.dependencies {
 			implementation(kotlin("test-junit5"))
 			runtimeOnly(libs.junit)
-		}
-
-		jsMain.dependencies {
-			implementation(kotlin("stdlib-js"))
 		}
 
 		jsTest.dependencies {
@@ -58,9 +60,19 @@ kotlin {
 	}
 }
 
-rootProject.the<NodeJsRootExtension>().apply {
-	version = "22.12.0"
+tasks.withType<Test>().configureEach {
+	maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).coerceAtLeast(1)
 }
+
+// JS task configuration
+
+allprojects {
+	project.plugins.withType<NodeJsPlugin> {
+		project.the<NodeJsEnvSpec>().version = "22.12.0" // latest LTS
+	}
+}
+
+// JVM task configuration
 
 tasks.withType<Jar> {
 	from(rootDir.resolve("LICENSE-APACHE")) {
@@ -70,8 +82,4 @@ tasks.withType<Jar> {
 	from(rootDir.resolve("LICENSE-MIT")) {
 		into("META-INF")
 	}
-}
-
-tasks.withType<Test>().configureEach {
-	maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).coerceAtLeast(1)
 }
